97820e409bfdfe943777a61c862afb5b18690f03
mchaldebas <mchaldebas@rockefeller.edu>
2024-11-16 18:06:11 -0500

    reorganizied

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import io
+import os
+
+# Read the contents of your README file
+current_directory = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(current_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='5ULTRA',
+        description='5ULTRA: A Pipeline for 5UTR Variant Annotation and Scoring',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        author='Matthieu Chaldebas',
+        author_email='mchaldebas@rockefeller.edu',
+        license='GPLv3',
+        url='https://github.com/mchaldebas/5ULTRA',
+        packages=['5ULTRA'],
+        install_requires=['pysam>=',
+                            'pandas>=',
+                            'numpy>=',
+                            'joblib>=',
+                            'sklearn>='],
+        package_data={'5ULTRA': ['data/*',
+                                'data/5UTR.hg38.phastCons100way/*',
+                                'data/5UTR.hg38.phyloP100way/*']},
+        entry_points={'console_scripts': ['5ULTRA=5ULTRA.__main__:main']},
+        classifiers=['Programming Language :: Python :: 3',
+                    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                    'Operating System :: OS Independent',
+                    ],
+        python_requires='>=3.6',
+        test_suite='tests')
